3b9d8ea1545abfd48e9f9607e6d084f2e80627bd
Arpit Mishra <arpitmishra61@gmail.com>
2020-01-12 02:59:16 +0530

    Updates

---
+++ precache-manifest.ea047c27e341453949917bed977829ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2232167f566ffe55f1bb8269285de733",
+    "url": "/videoplayeryoutube/index.html"
+  },
+  {
+    "revision": "009646819c5c670d259e",
+    "url": "/videoplayeryoutube/static/js/2.4c95d6f5.chunk.js"
+  },
+  {
+    "revision": "adf315857185901d18c2339ec58b6b66",
+    "url": "/videoplayeryoutube/static/js/2.4c95d6f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de88c46a10aaebcb4994",
+    "url": "/videoplayeryoutube/static/js/main.a4bb2e57.chunk.js"
+  },
+  {
+    "revision": "ad3224d02874de937c15",
+    "url": "/videoplayeryoutube/static/js/runtime-main.fdb4b63c.js"
+  }
+]);
