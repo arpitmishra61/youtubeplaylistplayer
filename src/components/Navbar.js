170e18de4e722f97d9c1d04a36781b99585ac900
Arpit Mishra <arpitmishra61@gmail.com>
2020-01-12 18:04:26 +0530

    dchanges-12

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-danger p-4">
 
-            <a href="#?" className="navbar-brand">
+            <a href="/" className="navbar-brand">
                 Video Player
             </a>
         </nav>
